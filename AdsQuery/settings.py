b5eba52c94b54d352698e4247687be71120943eb
Zaheer Abbas <nk4456542@gmail.com>
2020-12-28 03:01:06 +0530

    Start Ads App and test Basic Settings

--- AdsQuery/settings.py
+++ AdsQuery/settings.py
@@ -41,6 +41,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Ads.apps.AdsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
