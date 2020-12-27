8e1f96ba54017178be9242f2e7eb7e42dde16ca9
Zaheer Abbas <nk4456542@gmail.com>
2020-12-28 04:01:35 +0530

    Register Models on Admin site

--- Ads/admin.py
+++ Ads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Ads.models import Ad, Category, Comment, Fav
 
 # Register your models here.
+
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Fav)
